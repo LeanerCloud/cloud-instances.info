vantage-sh/ec2instances.info

feat(ec2): make reserved/saving plans dropdown now have two sections, instance saving plans and other savings

next/llms/generateAwsInstances.ts
@@ -16,6 +16,24 @@ const tableColumns = [
     ["yrTerm3Savings.noUpfront", "3yr No Upfront (Savings Plan)"],
     ["yrTerm3Savings.partialUpfront", "3yr Partial Upfront (Savings Plan)"],
     ["yrTerm3Savings.allUpfront", "3yr All Upfront (Savings Plan)"],
+    ["yrTerm1InstanceSavings.noUpfront", "1yr No Upfront (Instance Savings Plan)"],
+    [
+        "yrTerm1InstanceSavings.partialUpfront",
+        "1yr Partial Upfront (Instance Savings Plan)",
+    ],
+    [
+        "yrTerm1InstanceSavings.allUpfront",
+        "1yr All Upfront (Instance Savings Plan)",
+    ],
+    ["yrTerm3InstanceSavings.noUpfront", "3yr No Upfront (Instance Savings Plan)"],
+    [
+        "yrTerm3InstanceSavings.partialUpfront",
+        "3yr Partial Upfront (Instance Savings Plan)",
+    ],
+    [
+        "yrTerm3InstanceSavings.allUpfront",
+        "3yr All Upfront (Instance Savings Plan)",
+    ],
     ["yrTerm1Standard.noUpfront", "1yr No Upfront"],
     ["yrTerm1Standard.partialUpfront", "1yr Partial Upfront"],
     ["yrTerm1Standard.allUpfront", "1yr All Upfront"],
@@ -136,6 +154,12 @@ function generateInstanceMarkdown(
             case "yrTerm3Savings.noUpfront":
             case "yrTerm3Savings.partialUpfront":
             case "yrTerm3Savings.allUpfront":
+            case "yrTerm1InstanceSavings.noUpfront":
+            case "yrTerm1InstanceSavings.partialUpfront":
+            case "yrTerm1InstanceSavings.allUpfront":
+            case "yrTerm3InstanceSavings.noUpfront":
+            case "yrTerm3InstanceSavings.partialUpfront":
+            case "yrTerm3InstanceSavings.allUpfront":
             case "yrTerm1Standard.noUpfront":
             case "yrTerm1Standard.partialUpfront":
             case "yrTerm1Standard.allUpfront":

next/utils/dataMappings.ts
@@ -18,42 +18,75 @@ export const durationOptions: { value: CostDuration; label: string }[] = [
     { value: "annually", label: "Annually" },
 ] as const;
 
-const sharedReservedTermOptions = [
-    { value: "yrTerm1Standard.noUpfront", label: "1-year - No Upfront" },
+type ReservedTermOption = {
+    value: string;
+    label: string;
+    group?: string;
+};
+
+const OTHER_SAVINGS_GROUP = "Other Savings";
+const INSTANCE_SAVINGS_GROUP = "Instance Savings Plans";
+
+const sharedReservedTermOptions: ReservedTermOption[] = [
+    {
+        value: "yrTerm1Standard.noUpfront",
+        label: "1-year - No Upfront",
+        group: OTHER_SAVINGS_GROUP,
+    },
     {
         value: "yrTerm1Standard.partialUpfront",
         label: "1-year - Partial Upfront",
+        group: OTHER_SAVINGS_GROUP,
     },
-    { value: "yrTerm1Standard.allUpfront", label: "1-year - Full Upfront" },
-    { value: "yrTerm3Standard.noUpfront", label: "3-year - No Upfront" },
+    {
+        value: "yrTerm1Standard.allUpfront",
+        label: "1-year - Full Upfront",
+        group: OTHER_SAVINGS_GROUP,
+    },
+    {
+        value: "yrTerm3Standard.noUpfront",
+        label: "3-year - No Upfront",
+        group: OTHER_SAVINGS_GROUP,
+    },
     {
         value: "yrTerm3Standard.partialUpfront",
         label: "3-year - Partial Upfront",
+        group: OTHER_SAVINGS_GROUP,
     },
-    { value: "yrTerm3Standard.allUpfront", label: "3-year - Full Upfront" },
+    {
+        value: "yrTerm3Standard.allUpfront",
+        label: "3-year - Full Upfront",
+        group: OTHER_SAVINGS_GROUP,
+    },
     {
         value: "yrTerm1Convertible.noUpfront",
         label: "1-year convertible - No Upfront",
+        group: OTHER_SAVINGS_GROUP,
     },
     {
         value: "yrTerm1Convertible.partialUpfront",
         label: "1-year convertible - Partial Upfront",
+        group: OTHER_SAVINGS_GROUP,
     },
     {
         value: "yrTerm1Convertible.allUpfront",
         label: "1-year convertible - Full Upfront",
+        group: OTHER_SAVINGS_GROUP,
     },
     {
         value: "yrTerm3Convertible.noUpfront",
         label: "3-year convertible - No Upfront",
+        group: OTHER_SAVINGS_GROUP,
     },
     {
         value: "yrTerm3Convertible.partialUpfront",
         label: "3-year convertible - Partial Upfront",
+        group: OTHER_SAVINGS_GROUP,
     },
     {
         value: "yrTerm3Convertible.allUpfront",
         label: "3-year convertible - Full Upfront",
+        group: OTHER_SAVINGS_GROUP,
     },
 ];
 
@@ -64,9 +97,21 @@ const savingsPlanExtras = {
     "yrTerm3Savings.noUpfront": "3-year Savings Plan - No Upfront",
     "yrTerm3Savings.partialUpfront": "3-year Savings Plan - Partial Upfront",
     "yrTerm3Savings.allUpfront": "3-year Savings Plan - Full Upfront",
+    "yrTerm1InstanceSavings.noUpfront":
+        "1-year Instance Savings Plan - No Upfront",
+    "yrTerm1InstanceSavings.partialUpfront":
+        "1-year Instance Savings Plan - Partial Upfront",
+    "yrTerm1InstanceSavings.allUpfront":
+        "1-year Instance Savings Plan - Full Upfront",
+    "yrTerm3InstanceSavings.noUpfront":
+        "3-year Instance Savings Plan - No Upfront",
+    "yrTerm3InstanceSavings.partialUpfront":
+        "3-year Instance Savings Plan - Partial Upfront",
+    "yrTerm3InstanceSavings.allUpfront":
+        "3-year Instance Savings Plan - Full Upfront",
 };
 
-const savingsPlanCache = new Map<string, { value: string; label: string }[]>();
+const savingsPlanCache = new Map<string, ReservedTermOption[]>();
 
 export type SupportedSavingsPlanOptions = keyof typeof savingsPlanExtras;
 
@@ -78,14 +123,30 @@ export const reservedTermOptions = (
     const cached = savingsPlanCache.get(key);
     if (cached) return cached;
 
-    const extras: { value: string; label: string }[] = [];
-    for (const sp of savingsPlanSupported) {
-        const label = savingsPlanExtras[sp];
-        if (label) {
-            extras.push({ value: sp, label });
+    const instanceExtras: ReservedTermOption[] = [];
+    const otherExtras: ReservedTermOption[] = [];
+    for (const savingsPlan of savingsPlanSupported) {
+        const label = savingsPlanExtras[savingsPlan];
+        if (!label) continue;
+        const option = {
+            value: savingsPlan,
+            label,
+            group: savingsPlan.includes("InstanceSavings")
+                ? INSTANCE_SAVINGS_GROUP
+                : OTHER_SAVINGS_GROUP,
+        };
+        if (option.group === INSTANCE_SAVINGS_GROUP) {
+            instanceExtras.push(option);
+        } else {
+            otherExtras.push(option);
         }
+        // Put all the possible savingPlans into groups for the dropdown
     }
-    const options = [...extras, ...sharedReservedTermOptions];
+    const options = [
+        ...sharedReservedTermOptions,
+        ...instanceExtras,
+        ...otherExtras,
+    ];
     savingsPlanCache.set(key, options);
     return options;
 };

LeanerCloud/cloud-instances.info

next/llms/generateAwsInstances.ts
@@ -10,12 +10,42 @@ const tableColumns = [
     ["spot_avg", "Spot Avg"],
     ["spot_max", "Spot Max"],
     ["pct_interrupt", "Spot Interrupt Frequency"],
-    ["yrTerm1Savings.noUpfront", "1yr No Upfront (Savings Plan)"],
-    ["yrTerm1Savings.partialUpfront", "1yr Partial Upfront (Savings Plan)"],
-    ["yrTerm1Savings.allUpfront", "1yr All Upfront (Savings Plan)"],
-    ["yrTerm3Savings.noUpfront", "3yr No Upfront (Savings Plan)"],
-    ["yrTerm3Savings.partialUpfront", "3yr Partial Upfront (Savings Plan)"],
-    ["yrTerm3Savings.allUpfront", "3yr All Upfront (Savings Plan)"],
+    ["yrTerm1Savings.noUpfront", "1yr No Upfront (Compute Savings Plan)"],
+    [
+        "yrTerm1Savings.partialUpfront",
+        "1yr Partial Upfront (Compute Savings Plan)",
+    ],
+    ["yrTerm1Savings.allUpfront", "1yr All Upfront (Compute Savings Plan)"],
+    ["yrTerm3Savings.noUpfront", "3yr No Upfront (Compute Savings Plan)"],
+    [
+        "yrTerm3Savings.partialUpfront",
+        "3yr Partial Upfront (Compute Savings Plan)",
+    ],
+    ["yrTerm3Savings.allUpfront", "3yr All Upfront (Compute Savings Plan)"],
+    [
+        "yrTerm1InstanceSavings.noUpfront",
+        "1yr No Upfront (Instance Savings Plan)",
+    ],
+    [
+        "yrTerm1InstanceSavings.partialUpfront",
+        "1yr Partial Upfront (Instance Savings Plan)",
+    ],
+    [
+        "yrTerm1InstanceSavings.allUpfront",
+        "1yr All Upfront (Instance Savings Plan)",
+    ],
+    [
+        "yrTerm3InstanceSavings.noUpfront",
+        "3yr No Upfront (Instance Savings Plan)",
+    ],
+    [
+        "yrTerm3InstanceSavings.partialUpfront",
+        "3yr Partial Upfront (Instance Savings Plan)",
+    ],
+    [
+        "yrTerm3InstanceSavings.allUpfront",
+        "3yr All Upfront (Instance Savings Plan)",
+    ],
     ["yrTerm1Standard.noUpfront", "1yr No Upfront"],
     ["yrTerm1Standard.partialUpfront", "1yr Partial Upfront"],
     ["yrTerm1Standard.allUpfront", "1yr All Upfront"],
@@ -136,6 +166,12 @@ function generateInstanceMarkdown(
             case "yrTerm3Savings.noUpfront":
             case "yrTerm3Savings.partialUpfront":
             case "yrTerm3Savings.allUpfront":
+            case "yrTerm1InstanceSavings.noUpfront":
+            case "yrTerm1InstanceSavings.partialUpfront":
+            case "yrTerm1InstanceSavings.allUpfront":
+            case "yrTerm3InstanceSavings.noUpfront":
+            case "yrTerm3InstanceSavings.partialUpfront":
+            case "yrTerm3InstanceSavings.allUpfront":
             case "yrTerm1Standard.noUpfront":
             case "yrTerm1Standard.partialUpfront":
             case "yrTerm1Standard.allUpfront":

next/utils/dataMappings.ts
@@ -18,57 +18,120 @@ export const durationOptions: { value: CostDuration; label: string }[] = [
     { value: "annually", label: "Annually" },
 ] as const;
 
-const sharedReservedTermOptions = [
-    { value: "yrTerm1Standard.noUpfront", label: "1-year - No Upfront" },
+type ReservedTermOption = {
+    value: string;
+    label: string;
+    group?: string;
+};
+
+const RESERVED = "Reserved";
+const INSTANCE_SAVINGS_GROUP = "Instance Savings Plan";
+const COMPUTE_SAVINGS_PLAN = "Compute Savings Plan";
+
+const sharedReservedTermOptions: ReservedTermOption[] = [
+    {
+        value: "yrTerm1Standard.noUpfront",
+        label: "1-year - No Upfront",
+        group: RESERVED,
+    },
     {
         value: "yrTerm1Standard.partialUpfront",
         label: "1-year - Partial Upfront",
+        group: RESERVED,
     },
-    { value: "yrTerm1Standard.allUpfront", label: "1-year - Full Upfront" },
-    { value: "yrTerm3Standard.noUpfront", label: "3-year - No Upfront" },
+    {
+        value: "yrTerm1Standard.allUpfront",
+        label: "1-year - Full Upfront",
+        group: RESERVED,
+    },
+    {
+        value: "yrTerm3Standard.noUpfront",
+        label: "3-year - No Upfront",
+        group: RESERVED,
+    },
     {
         value: "yrTerm3Standard.partialUpfront",
         label: "3-year - Partial Upfront",
+        group: RESERVED,
     },
-    { value: "yrTerm3Standard.allUpfront", label: "3-year - Full Upfront" },
+    {
+        value: "yrTerm3Standard.allUpfront",
+        label: "3-year - Full Upfront",
+        group: RESERVED,
+    },
     {
         value: "yrTerm1Convertible.noUpfront",
         label: "1-year convertible - No Upfront",
+        group: RESERVED,
     },
     {
         value: "yrTerm1Convertible.partialUpfront",
         label: "1-year convertible - Partial Upfront",
+        group: RESERVED,
     },
     {
         value: "yrTerm1Convertible.allUpfront",
         label: "1-year convertible - Full Upfront",
+        group: RESERVED,
     },
     {
         value: "yrTerm3Convertible.noUpfront",
         label: "3-year convertible - No Upfront",
+        group: RESERVED,
     },
     {
         value: "yrTerm3Convertible.partialUpfront",
         label: "3-year convertible - Partial Upfront",
+        group: RESERVED,
     },
     {
         value: "yrTerm3Convertible.allUpfront",
         label: "3-year convertible - Full Upfront",
+        group: RESERVED,
     },
 ];
 
 const savingsPlanExtras = {
-    "yrTerm1Savings.noUpfront": "1-year Savings Plan - No Upfront",
-    "yrTerm1Savings.partialUpfront": "1-year Savings Plan - Partial Upfront",
-    "yrTerm1Savings.allUpfront": "1-year Savings Plan - Full Upfront",
-    "yrTerm3Savings.noUpfront": "3-year Savings Plan - No Upfront",
-    "yrTerm3Savings.partialUpfront": "3-year Savings Plan - Partial Upfront",
-    "yrTerm3Savings.allUpfront": "3-year Savings Plan - Full Upfront",
+    "yrTerm1Savings.noUpfront": "1-year Compute Savings Plan - No Upfront",
+    "yrTerm1Savings.partialUpfront":
+        "1-year Compute Savings Plan - Partial Upfront",
+    "yrTerm1Savings.allUpfront": "1-year Compute Savings Plan - Full Upfront",
+    "yrTerm3Savings.noUpfront": "3-year Compute Savings Plan - No Upfront",
+    "yrTerm3Savings.partialUpfront":
+        "3-year Compute Savings Plan - Partial Upfront",
+    "yrTerm3Savings.allUpfront": "3-year Compute Savings Plan - Full Upfront",
+    "yrTerm1InstanceSavings.noUpfront":
+        "1-year Instance Savings Plan - No Upfront",
+    "yrTerm1InstanceSavings.partialUpfront":
+        "1-year Instance Savings Plan - Partial Upfront",
+    "yrTerm1InstanceSavings.allUpfront":
+        "1-year Instance Savings Plan - Full Upfront",
+    "yrTerm3InstanceSavings.noUpfront":
+        "3-year Instance Savings Plan - No Upfront",
+    "yrTerm3InstanceSavings.partialUpfront":
+        "3-year Instance Savings Plan - Partial Upfront",
+    "yrTerm3InstanceSavings.allUpfront":
+        "3-year Instance Savings Plan - Full Upfront",
 };
 
-const savingsPlanCache = new Map<string, { value: string; label: string }[]>();
+const savingsPlanCache = new Map<string, ReservedTermOption[]>();
 
 export type SupportedSavingsPlanOptions = keyof typeof savingsPlanExtras;
+
+/** Label for reserved-cost columns: RI vs Savings Plan, based on the selected term key. */
+export function commitmentTypeLabel(
+    term: string,
+): "Reserved" | "Instance Savings Plan" | "Compute Savings Plan" {
+    if (!term.includes("Savings")) {
+        return "Reserved";
+    }
+
+    if (term.includes("Instance")) {
+        return "Instance Savings Plan";
+    }
+
+    return "Compute Savings Plan";
+}
 
 export const reservedTermOptions = (
     savingsPlanSupported: SupportedSavingsPlanOptions[] | undefined,
@@ -78,14 +141,30 @@ export const reservedTermOptions = (
     const cached = savingsPlanCache.get(key);
     if (cached) return cached;
 
-    const extras: { value: string; label: string }[] = [];
-    for (const sp of savingsPlanSupported) {
-        const label = savingsPlanExtras[sp];
-        if (label) {
-            extras.push({ value: sp, label });
+    const instanceSavingsPlanGroup: ReservedTermOption[] = [];
+    const computeSavingsPlanGroup: ReservedTermOption[] = [];
+    for (const savingsPlan of savingsPlanSupported) {
+        const label = savingsPlanExtras[savingsPlan];
+        if (!label) continue;
+        const option = {
+            value: savingsPlan,
+            label,
+            group: savingsPlan.includes("InstanceSavings")
+                ? INSTANCE_SAVINGS_GROUP
+                : COMPUTE_SAVINGS_PLAN,
+        };
+        if (option.group === INSTANCE_SAVINGS_GROUP) {
+            instanceSavingsPlanGroup.push(option);
+        } else {
+            computeSavingsPlanGroup.push(option);
         }
+        // Put all the possible savingPlans into groups for the dropdown
     }
-    const options = [...extras, ...sharedReservedTermOptions];
+    const options = [
+        ...sharedReservedTermOptions,
+        ...instanceSavingsPlanGroup,
+        ...computeSavingsPlanGroup,
+    ];
     savingsPlanCache.set(key, options);
     return options;
 };